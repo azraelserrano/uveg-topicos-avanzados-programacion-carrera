5e7bc76deaa92e9d439bdc28e4dbbab5abd801ce
azraelserrano <serrano_mane92@outlook.com>
2022-03-24 13:44:29 -0600

    Initial project

---
+++ src/Runner.java
@@ -0,0 +1,26 @@
+
+public class Runner {
+
+      private String name;
+   private int speed;
+    
+    public String getName() {
+        return name;
+    }
+
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+   
+    public int getSpeed() {
+        return speed;
+    }
+
+   
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+ 
+}
---
+++ src/ThreadRunner.java
@@ -0,0 +1,6 @@
+
+public class ThreadRunner extends Thread {
+    
+    
+    
+}
